871eb8fedeb931477b660afd296698eb9cccab8e
raman <ramanvellore2018@gmail.com>
2020-12-25 14:29:11 +0530

    first commit

---
+++ store/urls.py
@@ -0,0 +1,38 @@
+"""store URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+
+from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("frontend.urls")), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#handler404 = 'frontend.views.custom_page_not_found_view'
+# handler500 = 'frontend.views.handler500'
+
+admin.site.site_header = "Web Design Company"
+admin.site.site_title = "DJANGO WEBSITE"
+admin.site.site_index_title = "Welcome to Raman's Admin Panel"
